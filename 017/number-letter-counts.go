af2d88fa25a3d6f129f5ba9147839dc10d2873ea
Christopher Garvis <cgarvis@gmail.com>
2014-01-14 14:48:30 -0500

    Fix solution for 17.
    It was adding a "and" on hundreds.

--- 017/number-letter-counts.go
+++ 017/number-letter-counts.go
@@ -7,16 +7,15 @@
 package main
 
 import "fmt"
+import "flag"
 
+var (
+    digits = make([]string, 0)
+    teens = make([]string, 0)
+    tens = make([]string, 0)
+)
 
 func int_to_words(i int) string {
-	digits := make([]string, 0)
-	digits = append(digits, "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
-	teens := make([]string, 0)
-	teens = append(teens, "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen")
-	tens := make([]string, 0)
-	tens = append(tens, "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety")
-
 	words := ""
 
 	if i < 10 {
@@ -28,7 +27,9 @@ func int_to_words(i int) string {
 		words += int_to_words(i%10)
 	} else if i < 1000 {
 		words += digits[i/100] + "hundred"
-		words += "and" + int_to_words(i%100)
+		if i % 100 != 0 {
+            words += "and" + int_to_words(i%100)
+        }
 	} else if i < 10000 {
 		words += digits[i/1000] + "thousand"
 		words += int_to_words(i%1000)
@@ -39,11 +40,18 @@ func int_to_words(i int) string {
 
 func main() {
 	count := 0
+	count_to := flag.Int("to", 1000, "number to count to")
 
-	for i := 1; i <= 1000; i++ {
+	flag.Parse()
+
+    digits = append(digits, "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+    teens = append(teens, "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen")
+    tens = append(tens, "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety")
+
+	for i := 1; i <= *count_to; i++ {
 		words := int_to_words(i)
 		count += len(words)
-		fmt.Println(fmt.Sprintf("%s %d", words, len(words)))
+		fmt.Println(fmt.Sprintf("%s %d %d", words, len(words), count))
 	}
 
 	fmt.Println(count)
